dfdcf7e4050fb9196202905d4a355a7b91c71b7e
Jacky Zhu <jackyzhu@buffalo.edu>
2021-04-18 07:30:14 -0400

    implemented functionality for multipart form data, started dealing with login credentials

--- website/httpserver.py
+++ website/httpserver.py
@@ -1,8 +1,13 @@
 import http.server
 import socketserver
+import cgi
+import pymongo
 
 PORT = 8000
 HOST = "localhost"
+mongoclient = pymongo.MongoClient("mongodb://localhost:27017/")
+storedUsers = mongoclient["users"]
+profiles = storedUsers["profiles"]
 
 def readFile(filename, type):
     fileContent = None
@@ -61,6 +66,19 @@ def pathLocation(path, self):
         self.send_response(404)
         self.end_headers()
 
+#if login query, checks credentials, if insert, checks if username exists and if not insert to DB
+def entryQuery(qtype, entry):    
+    if qtype == "login":
+        account = profiles.find({"name": { $eq: entry["name"]}, "pwd": { $eq: entry["pwd"]}})
+        if account.count() != 0:
+            return True
+    elif qtype == "insert":
+        account = profiles.find({"name": { $eq: entry["name"]}})
+        if account.count() == 0:
+            profiles.insert(json.dumps(entry))
+            return True
+    return False
+            
 
 class server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -69,12 +87,28 @@ class server(http.server.SimpleHTTPRequestHandler):
         return response
 
 
-    def do_POST(self):
+    def do_POST(self):        
         path = self.path
         length = int(self.headers.get("Content-Length"))
-        isMultipart = self.header.isMultipart()
-        boundary = self.headers.get_boundary()
-        body = self.rfile.read(length)
+        isMultipart = True if "multipart/form-data" in self.headers.get("Content-Type") else False
+        if isMultipart:
+            boundary = {'boundary': self.headers.get_boundary().encode()}
+            data = cgi.parse_multipart(self.rfile, boundary)
+            if path == "/enternewuser":
+                name = data["enternewuser"]
+                pwd = data["enternewpass"]
+                entry = {"name": name, "pwd": pwd}
+                inserted = entryQuery("insert", entry) #deal with front end warning depending on the boolean value, false means username already exists and cannot be duplicated
+            elif path == "/loginuser":
+                name = data["loginusername"]
+                pwd = data["loginuserpass"]
+                entry = {"name": name, "pwd": pwd}
+                exists = entryQuery("login", entry) #deal with front end warning depending on the boolean value, false means credentials dont exist or are wrong
+            elif path == "/uploadproject":            
+                image = data["enterprojectname"]
+                pDesc = data["comment"]
+                pName = data["uploadprojectimage"]
+        
 
 
 with socketserver.ThreadingTCPServer((HOST, PORT), server) as httpd:
